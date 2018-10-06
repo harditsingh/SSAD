a9224080b35c09cbfbeaa2f312cbcabe34d8f338
Hardit Singh <hardit@Hardits-MacBook-Pro.local>
2018-10-06 18:56:00 +0800

    setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'SSAD',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-184-16-205.eu-central-1.compute.amazonaws.com',
+      key: '~/.ssh/SSAD.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:harditsingh/SSAD.git',
+      path: '/home/ubuntu/server',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
