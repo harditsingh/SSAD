f10693e0e17ed2cf0d357cefc7512a033228c68f
Hardit Singh <hardit@outlook.com>
2018-10-09 12:30:28 +0800

    Changed public DNS

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-18-184-16-205.eu-central-1.compute.amazonaws.com',
+      host: 'ec2-18-196-60-178.eu-central-1.compute.amazonaws.com',
       key: '~/.ssh/SSAD.pem',
       ref: 'origin/master',
       repo: 'git@github.com:harditsingh/SSAD.git',
